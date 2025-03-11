ed3bb37c24d106a78f6df9ceb70d9b883cb93846
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-03-11 22:49:51 +0300

    Basic implementation

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use clap::{Parser};
+
+#[derive(Parser)]
+#[command(name = "onlivi")]
+struct Cli {
+    #[arg(short, long, help = "Filter errors within the specified line range")]
+    line_range: Option<String>,
+}
+
+pub fn run(args: Cli) -> String {
+    match args.line_range {
+        Some(line_range) => format!("Line range: {}", line_range),
+        None => "Empty line range".to_string()
+    }
+}
+
+fn main() {
+    let args = Cli::parse();
+    println!("{}", run(args));
+}
